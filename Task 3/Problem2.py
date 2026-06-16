8f11f03825b38bed56834def6fa181f45920a7d8
Mahdi Hasan Mehedi <129614659+Mahdi767@users.noreply.github.com>
2026-06-16 16:11:53 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,27 @@
 
+x =  input()
+y = input()
+
+d = int(input())
+
+
+def hm_d(s1,s2):
+    mis_cnt = 0
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            mis_cnt+=1
+    return mis_cnt
+
+def pattern_match(p,t,d):
+    pos = []
+    k =  len(p)
+    
+    for i in range(len(t)-k+1):
+        win = t[i:i+k]
+        
+        if hm_d(p,win) <=d:
+            pos.append(i)
+    return pos
+
+res = pattern_match(x,y,d)
+print(*res)
